d4b40033fb7874c429cbb8e1de5f5ba95efa951e
Landormi <maxjugu.albert@gmail.com>
2024-12-09 15:41:00 +0100

    monnaie

--- src/GraphComponent/graph.jsx
+++ src/GraphComponent/graph.jsx
@@ -1,24 +1,38 @@
 import '../App.css'
 import React, { useState } from 'react';
 import LineChart from './LineChart';
+import axios from 'axios';
 
 function Graph() {
 
     const [selectMon, setSelM] = useState("Dollars");
+    const [monnaies, setMonnaies] = useState(null);
 
     const handleChange = (event) => {
         const monnaie = event.target.value;
         setSelM(monnaie);
     };
 
+    // console.log(`test env : ${import.meta.env.REACT_APP_API_ADRESSE}`)
+    const fetchMonaies = async () => {
+        try{
+            const reponse = await axios.get(`${import.meta.env.REACT_APP_API_ADRESSE}/api/donnee`)
+            console.log(reponse)
+            setMonnaies(reponse.data)
+        } catch{
+
+        }
+    }
+    fetchMonaies()
+
     return (
         <>
         <div className='global_g'>
             <select name="monnaie" id="monnaie" onChange={handleChange} value={selectMon} className='selec'>
                 <option className='inp_g'>--Please choose an option--</option>
-                <option value="Yen">Yen</option>
-                <option value="Dollars">Dollars</option>
-                <option value="Livre">Livre</option>
+                {monnaies.map(monnaie => {
+                    return <option className={monnaie}>{monnaie}</option>
+                })}
             </select>
             <LineChart monnaie={selectMon}/>
         </div>
